10b3a779883d8f74dd7fa968fed92289ca4c4987
bdc4 <bdc4@stmarys-ca.edu>
2018-06-03 20:57:20 -0700

    Added gui

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 #Event Generator
 
 import dicemachine
-import easygui
+from easygui.easygui import *
 
 class Table:
     name = None
@@ -26,22 +26,29 @@ class Sector:
 
 class Controller:
     day = 0
-    
+    sector = 0
+
     def NextDay(self):
         self.day += 1
         dayRoll = dicemachine.RollD(20)
-        for span in SectorCurrent.eventRange:
+        for span in self.sector.eventRange:
             if dayRoll >= span.minVal and dayRoll <= span.maxVal:
-               return print(str(dayRoll)+": An event was triggered on the "+str(span.name)+" table!")
+               return msgbox("An event was triggered on the "+str(span.name)+" table!")
 
 class Subsystem:
     name = None
-    damage = None
+    damage = 0
     sid = None
 
     def __init__(self, name):
         self.name = name
         self.sid = name[:1]
+    
+    def GetSeverity(self):
+        if self.damage == 0:
+            return "No Damage"
+        else:
+            return "Severity Level "+str(self.damage)
         
 class MECS:
     name = None
@@ -54,7 +61,7 @@ class MECS:
 
 class Crew:
     name = None
-    hp = 7
+    hp = 10
     room = None
 
     def __init__(self, name, room):
@@ -76,47 +83,40 @@ class Crew:
             return "Dead"
     
     def ChangeRoom(self):
-
         while True:
-            
-            print("Where should "+self.name+" be moved?\n")
-            for i in range(0, len(ROOMS)):
-                print("("+str(i+1)+") "+ROOMS[i].name)
-            where = input("\nChoice: ")
-            print("")
-            where = int(where)-1
-            
-            if where in range(0, len(ROOMS)):
-                oldRoom = self.room.name
-                self.room = ROOMS[where]
-                print(self.name+" has been moved from "+oldRoom+" to "+self.room.name)
-                return
-
+            where = buttonbox("Where should "+self.name+" be moved?\n","Room Reassignment",MECS_LABELS)
+            for room in ROOMS:
+                if where == room.name:
+                    oldRoom = self.room.name
+                    self.room = room
+                    msgbox(self.name+" has been moved from "+oldRoom+" to "+self.room.name)
+                    return
             print("Room not found. Try again...")
 
 def ShipStatus():
+    textStr = ""
     for room in ROOMS:
-            print("-----\n"+room.name+"\n-----\n")
-            for sub in room.subsystems:
-                print(sub.name+" - "+sub.sid)
-            print()
+        textStr += ("-----\n"+room.name+"\n-----\n")
+        for sub in room.subsystems:
+            textStr += (sub.name+" Components:\n"+"    Damage: "+sub.GetSeverity()+"\n")
+        textStr += "\n"
+    textStr += ("\n-----\nCrew Status\n-----\n")
     for crew in CrewMembers:
-        print(crew.name+"\n-- Room: "+crew.room.name+"\n-- Health: "+crew.GetHealth()+"\n")
+        textStr += ("\n"+crew.name+"\n-- Room: "+crew.room.name+"\n-- Health: "+crew.GetHealth()+"\n")
+    
+    textbox(textStr)
         
 def MoveCrew():
     while True:
-        easygui.buttonbox('test')
-        print("Who would you like to move?\n")
-        for i in range(0, len(CrewMembers)):
-            print("("+str(i+1)+") "+CrewMembers[i].name+" - "+CrewMembers[i].room.name)
+        crewNames = []
+        for crew in CrewMembers:
+            crewNames.append(crew.name)
+        who = buttonbox("Who would you like to move?","Pick Crew",crewNames)
         
-        who = input("\nChoice: ")
-        print("")
-        who = int(who)-1
-        
-        if who in range(0, len(CrewMembers)):
-            CrewMembers[who].ChangeRoom()
-            return
+        for crew in CrewMembers:
+            if who == crew.name:
+                crew.ChangeRoom()
+                return
         
         print("Not a valid selection! Try again...")
 
@@ -127,47 +127,35 @@ Sector1.eventRange = [Range("Nothing",1,9),Range("Events",10,13),Range("Meetings
 
 #init Ship
 MECS_LABELS = ["Medbay","Engines","Comms","Systems"]
-ROOMS = ["","","",""]
-CrewMembers = ["","","",""]
+ROOMS = [None]*4
+CrewMembers = [None]*4
 
 #define global vars
 EVENT_TYPES = ["EVENTS","MEETINGS","OTHER"]
 GC = Controller()
-SectorCurrent = Sector1
+GC.sector = Sector1
+
+for i in range(0, len(MECS_LABELS)):
+    ROOMS[i] = MECS(MECS_LABELS[i])
+
+for i in range(0, len(ROOMS)):
+    CrewMembers[i] = Crew("Jenkins "+str(i+1),ROOMS[i])
+
+OPTIONS=["Continue","Crew Status","Move Crew Member"]
 
 def main():
-    
-    for i in range(0, len(MECS_LABELS)):
-        ROOMS[i] = MECS(MECS_LABELS[i])
-
-    for i in range(0, len(ROOMS)):
-        CrewMembers[i] = Crew("Jenkins "+str(i+1),ROOMS[i])
 
     action = ""
 
     while action != "exit":
         
-        print("-------------\nDay: "+str(GC.day)+"\n-------------")
-        print("What would you like to do?\n")
-        action = input("(1) Continue\n(2) Crew Status\n(3) Move Crew Member\n\nChoice: ")
-        print("")
+        action = buttonbox("What would you like to do?","Day: "+str(GC.day),OPTIONS)
 
-        #test = 3 #int 3
-        #test == 3 #bool with value of True
-        #test == 7 #bool with value False
-
-        if action == "1":
+        if action == OPTIONS[0]:
             GC.NextDay()
-        elif action == "2":
+        elif action == OPTIONS[1]:
             ShipStatus()
-        elif action == "3":
+        elif action == OPTIONS[2]:
             MoveCrew()
-        
 
-    '''print(SectorCurrent.name)
-    for i in SectorCurrent.eventRange:
-        print(i.name)
-        print(i.minVal)
-        print(i.maxVal)
-    '''
-        
+main()
